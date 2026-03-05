504ba5aa9ac93314d4b9fb18ec40d21b3e91e660
srivarsha <srivarshanethi@gmail.com>
2026-03-05 15:05:38 +0530

    Add page33.js with add and subtract functions

---
+++ part4/page33.js
@@ -0,0 +1,8 @@
+// Importing functions from another file
+import { add, subtract } from './calc.js';
+
+const result1 = add(5,3);
+console.log(result1);
+const result2 = subtract(5,3);
+console.log(result2);
+
