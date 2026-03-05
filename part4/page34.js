bc99af9c3452a4526d1e51237d47614b0658374c
srivarsha <srivarshanethi@gmail.com>
2026-03-05 17:20:04 +0530

    Update page34.js

--- part4/page34.js
+++ part4/page34.js
@@ -1,18 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Register</title>
-
-</head>
-<body>
-    <h1>Register form</h1>
-    <form action="/register" method="POST">
-        <p><input type="text" placeholder="email"></p>
-        <p><input type="password" placeholder="password"></p>
-        <p><input type="submit" value="Register"></p>
-    </form>
-    
-</body>
-</html>
+//npm i ejs(embedded javascript template)
+import express from "express";//express is a package that allows us to create a web server
+const app = express();
+app.set("view engine","ejs");//set the view engine to ejs, so that we can render ejs files
+app.set("views","views");//set the views directory to ./views, so that we can render ejs files from the views directory
+app.listen(8080,()=>console.log("Server is running on port 8080"));//start the server on port 8080
+//http://localhost:8080/ in browser to see the result
+// app.get("/login",(req,res)=>{
+//     res.render("login");//render the login.ejs file when the user visits the root URL
+// })
+app.get("/register",(req,res)=>{
+    res.render("register");//render the register.ejs file when the user visits the root URL
+})
